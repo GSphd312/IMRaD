85a07e92ae6eb03277d6b5d910fcace7576c87d4
GSphd312 <hper.h263@gmail.com>
2026-09-18 23:57:35 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase configuration for the NEW four-article website only.
+window.IMRAD_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCrEd_Xy8QvJoLUHjkbuz8sBHD0JlYQ4wU",
+  authDomain: "imrad-four-articles-class.firebaseapp.com",
+  projectId: "imrad-four-articles-class",
+  storageBucket: "imrad-four-articles-class.firebasestorage.app",
+  messagingSenderId: "860526745334",
+  appId: "1:860526745334:web:3c5580473fba60d8943ed7"
+};
